97a63487540203bbf6e8e1c666146ad15ff20dd1
danielOuattara <daniel.ouattara@gmx.com>
2023-06-01 14:42:48 +0200

    _03 data type: starting

---
+++ src/_03_data_type.rs
@@ -0,0 +1,17 @@
+pub fn run() {
+    // primitive types
+    /*
+    integers
+    floats
+    boolean
+    characters
+    Tuples
+    Arrays
+
+     */
+    println!("");
+
+    println!("-------------------------------------------");
+}
+
+// Stopped @ 28'
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod _01_print;
+mod _02_var;
+mod _03_data_type;
+
+fn main() {
+    _01_print::run();
+    _02_var::run();
+    _03_data_type::run();
+}
