1c817945de4006fb22b69d9151494bd4bfc073e3
danielOuattara <daniel.ouattara@gmx.com>
2023-06-02 09:20:01 +0200

    main updated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-mod _01_print;
-mod _02_var;
-mod _03_data_type;
+// mod _01_print;
+// mod _02_var;
+// mod _03_data_type;
+mod _04_strings;
 
 fn main() {
-    _01_print::run();
-    _02_var::run();
-    _03_data_type::run();
+    // _01_print::run();
+    // _02_var::run();
+    // _03_data_type::run();
+    _04_strings::run()
 }
+
+// Stopped @00h28'
+// Stopped @00h41'37'
