b6ab23b34071f4552fdc7523acf9cd59b310110e
danielOuattara <daniel.ouattara@gmx.com>
2023-06-20 23:18:21 +0200

    module renamed + logic  updated

--- src/_02_variables.rs
+++ src/_02_variables.rs
@@ -2,6 +2,7 @@ pub fn run() {
     // immutable variable
     let name = "John Doe";
     println!("{}", name);
+    println!("{name}"); // recent syntax
 
     // mutable variable
     let mut age = 37;
@@ -13,6 +14,7 @@ pub fn run() {
     // constant
     const ID: i32 = 007;
     println!("ID: {}", ID);
+    println!("ID: {ID}");
 
     // Assign multiple variables at once
     let (country, has_children, is_married, number_of_car) = ("France", true, true, 2);
@@ -20,6 +22,8 @@ pub fn run() {
     println!(
         "I live in {}. children with me: {}, Married : {}, Number of car(s) {}",
         country, has_children, is_married, number_of_car
+    );
+    println!("I live in {country}. children with me: {has_children}, Married : {is_married}, Number of car(s) {number_of_car}"
     );
 
     println!("-------------------------------------------");
