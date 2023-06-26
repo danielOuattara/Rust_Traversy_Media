fd6a87ee3a1d2780badad660d1b0a262f4581ee5
danielOuattara <daniel.ouattara@gmx.com>
2023-06-26 22:53:32 +0200

    12 structs

---
+++ src/_12_structs.rs
@@ -0,0 +1,107 @@
+// Structs : use to create custom data types
+
+/*  Traditional Struct : 1
+-------------------------- */
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+/* tuple Struct : : 2
+---------------------- */
+struct ColorTuple(u8, u8, u8);
+
+/* struct with function implementation : 3
+------------------------------------------- */
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    // construct a person
+    fn create_person(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+
+    // get person full name
+    fn get_full_name(&self) {
+        let full_name = format!("The full name is {} {}", self.first_name, self.last_name);
+        println!("{}", full_name);
+    }
+
+    // update person first name
+
+    fn update_first_name(&mut self, new_first_name: String) {
+        self.first_name = new_first_name
+    }
+
+    fn update_last_name(&mut self, new_last_name: String) {
+        self.last_name = new_last_name
+    }
+}
+
+//-----------------------------------------------------------------------
+
+pub fn run() {
+    /* --- 1 */
+    let mut color_1 = Color {
+        red: 255,
+        green: 0,
+        blue: 0,
+    };
+
+    println!(
+        "color_1 is rgb({}, {}, {})",
+        color_1.red, color_1.green, color_1.blue
+    );
+
+    color_1.red = 200;
+    println!(
+        "color_1 is rgb({}, {}, {})",
+        color_1.red, color_1.green, color_1.blue
+    );
+
+    println!("------------------------------------------------");
+
+    /* --- 2 */
+    let mut color_tuple_1 = ColorTuple(0, 255, 0);
+
+    println!(
+        "color_tuple_1 is rgb({}, {}, {})",
+        color_tuple_1.0, color_tuple_1.1, color_tuple_1.2
+    );
+
+    color_tuple_1.0 = 200;
+
+    println!(
+        "color_tuple_1 is rgb({}, {}, {})",
+        color_tuple_1.0, color_tuple_1.1, color_tuple_1.2
+    );
+
+    println!("-----------------------------------------------");
+
+    /* --- 3 */
+    let person_1 = Person {
+        first_name: "Jana".to_string(),
+        last_name: String::from("Doe"),
+    };
+
+    println!("person_1 is {} {}", person_1.first_name, person_1.last_name);
+
+    let mut person_2 = Person::create_person("John", "Doe");
+    println!("person_2 is {} {}", person_2.first_name, person_2.last_name);
+
+    person_2.get_full_name(); // John Doe
+
+    person_2.update_first_name("Simon".to_owned());
+    person_2.get_full_name(); // John Simon
+
+    person_2.update_last_name("Django".to_owned());
+    person_2.get_full_name(); //  Simon Django
+}
