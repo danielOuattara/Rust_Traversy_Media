c117b12bd2649b38787542c18734e8fc459a2e8a
danielOuattara <daniel.ouattara@gmx.com>
2023-06-26 22:53:50 +0200

    main funct° updated

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,30 @@ cargo build --release: --> a ready for deployment
 
 // mod _01_print;
 // mod _02_variables;
-mod _03_data_type;
+// mod _03_data_type;
 // mod _04_strings;
+// mod _05_tuples;
+// mod _06_arrays;
+// mod _07_vectors;
+// mod _08_conditional;
+// mod _09_loops;
+// mod _10_functions;
+// mod _11_pointer_ref;
+mod _12_structs;
 
 fn main() {
     // _01_print::run();
     // _02_variables::run();
-    _03_data_type::run();
-    // _04_strings::run()
+    // _03_data_type::run();
+    // _04_strings::run();
+    // _05_tuples::run()
+    // _06_arrays::run(); // 00H48
+    // _07_vectors::run(); // 00H55:18
+    // _08_conditional::run(); // 01H00:25
+    // _09_loops::run(); // 01H07:00
+    // _10_functions::run(); // 01H13:31
+    // _11_pointer_ref::run(); // 01H13:31
+    _12_structs::run(); // 01H23:00
 }
 
-// Stopped @00h28'
-// Stopped @00h41'37'
+// STOPPED @01h33
